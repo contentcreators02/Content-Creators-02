f3496037a6a0b9df34d7673c7c19acc22ed45be3
contentcreators02 <jrjewelislamr@gmail.com>
2025-06-02 20:45:55 +0600

    delete old file

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// Replace these with your own Firebase config
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-app-id",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "APP_ID"
-};
 
-firebase.initializeApp(firebaseConfig);
